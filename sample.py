e80f5e24ac6614fb70e9cd47c7a8e6dc65337b4e
elyaziaa <ea2749@nyu.edu>
2025-02-27 13:42:12 +0400

    changed 2

--- sample.py
+++ sample.py
@@ -2,5 +2,5 @@ def greet(name):
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    user_name = input("Enter your name: Elyazia CHANGED")
+    user_name = input("Enter your name: Elyazia CHANGED CHANGED AGAIN")
     print(greet(user_name))
